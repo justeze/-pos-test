dfcc1b7ffeef0f9a9f6d45a3a22c4d3e9d6544bd
Faiz <65412644+justeze@users.noreply.github.com>
2020-10-18 10:32:37 +0700

    create 1

---
+++ 1.js
@@ -0,0 +1,14 @@
+const sortArray = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortArray(["z", "d", "a", "i"]));
